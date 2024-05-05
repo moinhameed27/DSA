e421844c1697d4337171bc3b758c04bfe4179dcb
moinhameed27 <moinhameed27@gmail.com>
2024-05-05 18:09:29 +0500

    Check Sorted Array

---
+++ Arrays/03_Check Sorted Array.cpp
@@ -0,0 +1,11 @@
+// Coding Ninjas : https://www.codingninjas.com/codestudio/problems/ninja-and-the-sorted-check_6581957?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+int isSorted(int n, vector<int> a)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > a[i + 1])
+            return false;
+    }
+    return true;
+}
