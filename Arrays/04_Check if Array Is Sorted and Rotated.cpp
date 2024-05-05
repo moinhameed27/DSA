d0831a82aa2e85dffd72cad7cb942597b48c44b9
moinhameed27 <moinhameed27@gmail.com>
2024-05-05 20:24:59 +0500

    Check if Array Is Sorted and Rotated

--- Arrays/04_Check if Array Is Sorted and Rotated.cpp
+++ Arrays/04_Check if Array Is Sorted and Rotated.cpp
@@ -1,9 +1,10 @@
-// LeetCode : https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/#:~:text=Input%3A%20nums%20%3D%20%5B2%2C,no%20rotation)%20to%20make%20nums.
+LeetCode : https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/#:~:text=Input%3A%20nums%20%3D%20%5B2%2C,no%20rotation)%20to%20make%20nums.
 
 /*
 If the array is sorted and rotated by some k elmements, to check if it's sorted or not we need to check it's
 break points. If the array was sorted originally, then it won't have more than one break point.
 */
+
 class Solution
 {
 public:
