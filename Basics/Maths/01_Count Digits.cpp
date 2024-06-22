195cbf522fdf006171cb0d16e99d47f4ef53b592
moinhameed27 <moinhameed27@gmail.com>
2024-06-22 23:16:46 +0500

    Count Digits

--- Basics/Maths/01_Count Digits.cpp
+++ Basics/Maths/01_Count Digits.cpp
@@ -1,5 +1,6 @@
 // GFG : https://www.geeksforgeeks.org/problems/count-digits5716/1
 
+// Given a number N. Count the number of digits in N which evenly divide N.
 class Solution
 {
 public:
@@ -17,3 +18,26 @@ public:
         return count;
     }
 };
+
+// Count the Number of Digits in a number
+// Way - I (Brute Force)
+int countDigits(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        count++;
+        n /= 10;
+    }
+    return count;
+}
+
+// Way - II (Optimal [Log])
+int countDigits(int n)
+{
+
+    int count = (int)(log10(n) + 1);
+    // The expression (int)(log10(n)+1) calculates the number of digits in 'n' and casts it to an integer.
+    // Adding 1 to the result accounts for the case when 'n' is a power of 10, // ensuring that the count is correct.
+    return count;
+}
