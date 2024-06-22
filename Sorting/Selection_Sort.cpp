f2cc44e89ef3eb79d0e66a724ff16372422a6ea4
moinhameed27 <moinhameed27@gmail.com>
2024-06-22 12:24:39 +0500

    Selection Sort

---
+++ Sorting/Selection_Sort.cpp
@@ -0,0 +1,29 @@
+// Select the minimum and swap [Push minimum to the start]
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Selection_Sort(vector<int> &v, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int mini = i;
+        for (int j = i; j < n; j++)
+        {
+            if (v[j] < v[mini])
+                mini = j;
+        }
+        swap(v[i], v[mini]);
+    }
+}
+
+int main()
+{
+    vector<int> v = {2, 3, 1, 4, 5};
+    int n = v.size();
+    Selection_Sort(v, n);
+    for (int i = 0; i < n; i++)
+        cout << v[i] << " ";
+
+    return 0;
+}
