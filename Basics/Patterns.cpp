a242068b707919da730c75b91753f7fdc70faf77
moinhameed27 <moinhameed27@gmail.com>
2024-06-24 17:37:35 +0500

    Patterns

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -209,8 +209,140 @@ void pattern10(int n)
 0 1
 1 0 1
 0 1 0 1
-
+1 0 1 0 1
 */
+void pattern11(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        bool flag = false;
+        if(i % 2 == 0)
+            flag = true;
+        for(int j = 0; j <= i; j++)
+        {
+            flag == true ? cout << "1 " : cout << "0 ";
+            flag = !flag;
+        }
+        cout << endl;
+    }
+}
+
+/*
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+*/
+void pattern12(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <= i; j++)
+            cout << j + 1;
+        for(int k = 0; k < 2 * n - (i * 2) - 2; k++)
+            cout << " ";
+        for(int j = i + 1; j > 0; j--)
+            cout << j;
+        cout << endl;
+    }
+}
+
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+void pattern13(int n)
+{
+    int num = 1;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <= i; j++)
+            cout << num++ << " ";
+
+        cout << endl;
+    }
+}
+
+/*
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+void pattern14(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        char ch = 'A';
+        for(int j = 0; j <= i; j++)
+            cout << ch++;
+        cout << endl;
+    }
+}
+
+/*
+ABCDE
+ABCD
+ABC
+AB
+A
+*/
+void pattern15(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        char ch = 'A';
+        for(int j = n; j > i; j--)
+            cout << ch++;
+        cout << endl;
+    }
+}
+
+/*
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+void pattern16(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for(int j = 0; j <= i; j++)
+            cout << ch;
+        cout << endl;
+    }
+}
+
+/*
+    A
+   ABA
+  ABCBA
+ ABCDCBA
+ABCDEDCBA
+*/
+void pattern17(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+            cout << " ";
+        char ch = 'A';
+        for(int k = 0; k <= i; k++)
+            cout << ch++;
+        ch -= 2;
+        for(int l = i; l > 0; l--)
+            cout << ch--;
+        cout << endl;
+    }
+}
 
 
 int main()
@@ -235,6 +367,20 @@ int main()
     pattern9(n);
     cout << endl;
     pattern10(n);
+    cout << endl;
+    pattern11(n);
+    cout << endl;
+    pattern12(n);
+    cout << endl;
+    pattern13(n);
+    cout << endl;
+    pattern14(n);
+    cout << endl;
+    pattern15(n);
+    cout << endl;
+    pattern16(n);
+    cout << endl;
+    pattern17(n);
     cout << endl;
     return 0;
 }
